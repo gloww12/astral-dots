80b8512948604ce5128292b6be7bc456d07a0025
gloww12 <glow@glow12.net>
2025-10-04 02:25:43 +0100

    feat: nv binds

--- .config/nvim/lua/config/binds.lua
+++ .config/nvim/lua/config/binds.lua
@@ -1,4 +1,6 @@
 local map = vim.keymap.set
+
+
 map('n', ';', ':')
 map('n', '<C-t>', '<cmd>ToggleTerm<CR>')
 map('t', '<Esc>', '<C-\\><C-n>')
@@ -8,7 +10,21 @@ map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-l>', '<C-w>l')
 
-map('n', 'qq', '<cmd>q<CR>')
+map('n', 'qq', 
+  function()
+    vim.cmd('NvimTreeClose')
+    vim.cmd('q')
+    vim.cmd('q')
+    vim.cmd('q')
+    vim.cmd('q')
+    vim.cmd('q')
+    vim.cmd('q')
+    vim.cmd('q')
+    vim.cmd('q')
+    vim.cmd('q')
+    -- it works ok dont judge me ;-;
+  end
+)
 
 -- nvimtree
 map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
